9fabbcbca07a6c7e5190db70b09eed379af7be41
Jesper Karlsson <jesper.karlsson@scania.com>
2018-01-29 14:51:35 +0100

    Added tabs

--- docs/ux-lib.js
+++ docs/ux-lib.js
@@ -1,4 +1,26 @@
 
+$(function(){
+
+	$(".public-styling").click(function() {
+	    $('body').removeClass("app");
+	    $('body').addClass("public");  
+	    $('.app-styling').removeClass("active"); 
+	    $('.public-styling').addClass("active");   
+	    
+	});
+
+	$(".app-styling").click(function() {
+	    $('body').removeClass("public");
+	    $('body').addClass("app");        
+	    $('.app-styling').addClass("active"); 
+	    $('.public-styling').removeClass("active");     
+	});  
+
+});        
+
+    
+
+
 setTimeout(function(){ document.querySelector("html").style.display = 'opacity'; }, 0);
 
 // Triggers when corporate-ui has finnised loading
