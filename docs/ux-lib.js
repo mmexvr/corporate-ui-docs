bb37fb121b7dbc407d595631ea6f51a7f12e695e
Jesper Karlsson <jesper.karlsson@scania.com>
2018-07-19 14:58:02 +0200

    Added Tabs Script

--- docs/ux-lib.js
+++ docs/ux-lib.js
@@ -1,11 +1,6 @@
 
   $(function(){
 
-    var url = document.location.toString();
-    	if (url.match('#')) {
-        $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
-    }
-
     $(".public-styling").click(function() {
       $('.zone').removeClass("app");
       $('.zone').addClass("public");  
@@ -22,6 +17,29 @@
     });         
 
   });   
+
+
+  function bootstrap_tab_bookmark (selector) { if (selector == undefined) {
+    selector = ""; }
+
+    /* Automagically jump on good tab based on anchor */
+    $(document).ready(function() {
+        url = document.location.href.split('#');
+        if(url[1] != undefined) {
+            $(selector + '[href=#'+url[1]+']').tab('show');
+        }
+    });
+
+    var update_location = function (event) {
+        document.location.hash = this.getAttribute("href");
+    }
+
+    /* Update hash based on tab */
+    $(selector + "[data-toggle=pill]").click(update_location);
+    $(selector + "[data-toggle=tab]").click(update_location);
+}
+
+
 
 
 
