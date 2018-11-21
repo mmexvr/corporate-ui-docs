ebef042ee06103add82d07f4c774ef29d5606ad9
Jesper Karlsson <jesper.karlsson@scania.com>
2018-11-21 10:09:01 +0100

    Fixes

--- docs/ux-lib.js
+++ docs/ux-lib.js
@@ -39,27 +39,3 @@
     $(selector + "[data-toggle=tab]").click(update_location);
 }
 
-
-
-
-
-
-// setTimeout(function(){ document.querySelector("html").style.display = 'opacity'; }, 0);
-
-// // Triggers when corporate-ui has finnised loading
-
-// document.addEventListener('corporate-ui-loaded', function(e) {
-
-//   document.body.className += ' done-loading';
-
-
-// });
-
-
-// setTimeout(function(){ 
-
-// var elm = document.querySelector('c-main-navigation')
-// elm.__proto__.setHeaderSize.call(elm)
-
-// }, 200);
-
