78bf9e1fde5eef838fd9a924dc68282cc18937ca
Jesper <jesperkarlsson@Jespers-iMac.lan>
2018-11-24 13:28:40 +0100

    Changed to modal-content

--- docs/developer/js/used-vehicles.js
+++ docs/developer/js/used-vehicles.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 const bodyScrollLock = require('bodyScrollLock.js');
 
-const scrollTarget = document.querySelector('.modal-body');
+const scrollTarget = document.querySelector('.modal-content');
 const disableBodyScrollButton = document.querySelector('.disableBodyScroll');
 const enableBodyScrollButton = document.querySelector('.enableBodyScroll');
 
